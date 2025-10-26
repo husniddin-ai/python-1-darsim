85294a2f78862e33d9fc9cb66d5a48b644c49ed5
husniddin <hamzaismoilov892@gmail.com>
2025-10-26 19:27:23 +0500

    lugatlar
    lugatlar yuklash

---
+++ untitled4.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 26 18:06:49 2025
+
+@author: hp_pc
+"""
+
+# Lug'at (Dictionary)
+#car_0 ={'model':'ferrari', 'rang':'qizil'}
+#print(car_0['model'])
+#print(car_0['rang'])
+
+en_uz = {'apple':'olma', 'apricot':"o\'rik", 'banana':'banan'}
+#print(en_uz['apple'])
+
+#mevalar = {'olma':'10000', 'tarvuz': '8000', 'qovun': 10000}
+#print(f"Olma narhi {mevalar['olma']} so\'m")
+
+#talaba_0 = {'ism':'murod olimov', 'yosh':'20', 't_yil':'2005'}
+#print(f"{talaba_0['ism'].title()}, \
+#    {talaba_0['t_yil']}, \
+#   {talaba_0['yosh']} yoshda")
+# Yangi kalit so'z qiymat qo'shish
+#talaba_0['kurs'] = 4
+#talaba_0['fakultet'] = 'informatika'
+#talaba_0['ism'] = 'abdulloh'
+
+#talaba_1 = {}
+#talaba_1['ism'] = 'qobil rasulov'
+#talaba_1['kurs'] = 3
+#talaba_1['yosh'] = '20'
+#print(talaba_1)
+#print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']} kurs")
+# qiymatni yangilash
+#talaba_1['kurs'] = 4
+#print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']} kurs")
+
+#kalit so'z qiymatni ochirib tashlash
+#talaba_0 = {'ism':'murod olimov', 'yosh':'20', 't_yil':'2005'}
+#del talaba_0['yosh']
+#print(talaba_0)
+
+#Lug'atlarni bir nechta qatorda yozish
+telefonlar = {
+    'ali':'iphone x',
+    'vali':'galaxy s9',
+    'olim':'mi 10 pro',
+    'orif':'nokia 3310',
+    'anvar':'pixel 3xl'
+    }
+
+#get() metodi
+#phone = telefonlar['ali']
+#print(f"Alining telefoni {phone}")
+      
+#phone = telefonlar.get("ali", "Bunday ismi mavjud emas")
+#print(phone)
+
+#meva = en_uz.get("pineapple", "Bunday ismi mavjud emas")
+#print(meva)
+
+#phone = telefonlar.get("hasan")
+#print(phone)
+
+
